c653f77268ff8f813028aca03d8d7e4d4f9ff54d
Tomka <32951703+tomkaboris@users.noreply.github.com>
2023-05-30 20:34:24 +0200

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
-FROM nginx:alpine
+FROM ubuntu 
 
-RUN apk add python3-dev=3.6.9-r3 --repository=http://dl-cdn.alpinelinux.org/alpine/v3.9/main && ln -sf python3 /usr/bin/python
-RUN python3 -m ensurepip
-RUN pip3 install --no-cache --upgrade pip setuptools
+RUN apt update
+RUN apt install -y apache2 
+RUN apt install -y apache2-utils 
+RUN apt install -y vim
+RUN apt install python3-pip -y
 
-COPY . /usr/share/nginx/html
+RUN pip install bs4
+RUN pip install requests
+
+COPY . /var/www/html/
+
+RUN cd /var/www/html && python3 fetchdata.py
+
+CMD ["apache2ctl", "-D", "FOREGROUND"]
+
---
+++ fetchdata.py
@@ -0,0 +1,41 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://docs.google.com/spreadsheets/d/1_VQoY4GYbuI2Cu9Pl8ahxTmYoTE3Rvke6CuBDXyHG04/edit#gid=0'
+response = requests.get(url)
+html_content = response.text
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find the table element containing the data
+table = soup.find('table')
+
+items = ['DevOps','Linux','PHP','K8s','Docker','AWS','Java','Python']
+values = []
+
+for i in range(8):
+
+    row_index = 2
+    column_index = i
+
+    # Find the row in the table
+    row = table.find_all('tr')[row_index]
+
+    # Find the specific cell in the row
+    cell = row.find_all('td')[column_index]
+
+    # Extract the content of the cell
+    cell_content = cell.text
+    values.append(cell_content)
+
+# jobs = dict(zip(values, items))
+jobs = [{ 'position': items, 'demand': values } for items, values in zip(items, values)]
+
+# Serializing json
+json_object = json.dumps(jobs, indent=8)
+
+# Writing to sample.json
+with open("jobs.json", "w") as outfile:
+    outfile.write(json_object)
